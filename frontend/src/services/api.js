8a1761631b62b8e8265f6609cdf9489542d00b22
nehareddy0705 <mittabaineha0705@gmail.com>
2026-06-05 16:00:58 +0530

    Update API baseURL to point to production Render server URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://taskmanager-ht2g.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
