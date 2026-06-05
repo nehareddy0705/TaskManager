d257512c7c334c4f51c0a0e6bb73759a81a1ddbb
nehareddy0705 <mittabaineha0705@gmail.com>
2026-06-05 16:05:24 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://task-manager-two-mu-63.vercel.app'],
     credentials: true
 }));
 
